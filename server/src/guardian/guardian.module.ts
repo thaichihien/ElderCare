4f86b6a77ab082d8f3a0c82a9dd02a79638c8a20
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-07-24 10:26:51 +0700

    update schedule

--- server/src/guardian/guardian.module.ts
+++ server/src/guardian/guardian.module.ts
@@ -3,11 +3,13 @@ import { GuardianController } from './guardian.controller';
 import { GuardianService } from './guardian.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Guardian, GuardianSchema } from './schemas/guardian.schema';
+import { Qualification, QualificationSchema } from './schemas/qualification.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Guardian.name, schema: GuardianSchema },
+      { name: Qualification.name, schema: QualificationSchema }
     ]),
   ],
   controllers: [GuardianController],
--- server/src/guardian/schemas/guardian.schema.ts
+++ server/src/guardian/schemas/guardian.schema.ts
@@ -1,5 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Qualification, QualificationSchema } from './qualification.schema';
 
+enum Level {
+  Amateur,
+  Professional,
+}
 
 // Định nghĩa bảng dữ liệu trong mongoDB
 @Schema({
@@ -18,6 +23,12 @@ export class Guardian {
   dateOfBirth: string;
   @Prop()
   address: string;
+
+  @Prop({type: String,enum: Level,default : Level.Amateur})
+  level: Level
+
+  @Prop({ type: [QualificationSchema] })
+  qualifications: Qualification[];
 }
 
-export const GuardianSchema = SchemaFactory.createForClass(Guardian)
+export const GuardianSchema = SchemaFactory.createForClass(Guardian);
